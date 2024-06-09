a838844ae0fdf57647da7ab4b62097dd69c31619
Wylliam Judd <wylliamjudd@meta.com>
2024-06-09 10:50:58 -0700

    characters

--- encounter.rb
+++ encounter.rb
@@ -1,13 +1,26 @@
+require 'yaml'
+require_relative 'characters/character'
+
 class Encounter
   attr_reader :monsters
 
-  def initialize monsters
-    @monsters = monsters
+  Monsters = YAML.load(File.read('yaml/monsters.yaml'))
+
+  def initialize keys
+    @monsters = create_monsters keys
   end
 
   def run party
-    p party.join ' '
+    p party
     p 'vs'
-    p monsters.join ' '
+    p monsters
+  end
+
+  private
+
+  def create_monsters keys
+    keys.map.with_index 1 do |key, i|
+      Character.new Monsters[key], "#{key.capitalize}-#{i}"
+    end
   end
 end
